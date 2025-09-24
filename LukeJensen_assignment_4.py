8fdc84b4a424f944e7553cbbf77a7419ff620404
Luke Jensen <lukewesten77@gmail.com>
2025-09-24 16:23:58 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ LukeJensen_assignment_4.py
@@ -0,0 +1,7 @@
+student_name = "Luke Jensen"
+current_gpa = 3.9
+study_hours = 5
+social_points = 25
+stress_level = 25
+
+print("Welcome, these are your Starting Stats", student_name, study_hours, social_points, stress_level)
