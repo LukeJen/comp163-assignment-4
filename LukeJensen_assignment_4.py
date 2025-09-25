2c93010240f89c3e6b2bc9d458c9d08a24fd7226
LukeJen <71953760+LukeJen@users.noreply.github.com>
2025-09-25 10:30:39 -0400

    Pass Test Case 2: Course planning with if/elif/else and comparison operators(Error Fix)

--- LukeJensen_assignment_4.py
+++ LukeJensen_assignment_4.py
@@ -11,8 +11,8 @@ if choice == "A":
         study_hours = study_hours - 6
         stress_level = stress_level - 30
     elif (current_gpa <= 3.4) and (current_gpa > 2.5):
-        study_hours - 3
-        stress_level - 20
+        study_hours = study_hours - 3
+        stress_level = stress_level - 20
     elif current_gpa < 2.4:
         print("Harder Path Recommened")
 elif choice == "B": 
