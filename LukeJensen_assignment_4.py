b28ed2bd2265f063a14e1716e28b6ef5e3bd482b
LukeJen <71953760+LukeJen@users.noreply.github.com>
2025-09-25 16:29:11 -0400

    choice variable missing from code (fix)

--- LukeJensen_assignment_4.py
+++ LukeJensen_assignment_4.py
@@ -4,6 +4,7 @@ current_gpa = 3.9
 study_hours = 5
 social_points = 25
 stress_level = 25
+choice = input()
 
 print("Welcome, these are your Starting Stats", student_name, study_hours, social_points, stress_level)
 print("Choose Your Path: A(Easy), B(Medium), or C(Hard)")
